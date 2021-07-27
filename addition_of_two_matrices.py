f13c56bb716eddbf0eb505dc2d8783ca201fda92
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-07-27 19:37:15 +0530

    handled exceptions to avoid abnormal terminations

---
+++ addition_of_two_matrices.py
@@ -0,0 +1,81 @@
+# Program to add two matrices
+import sys
+
+class AddingTwoMatrices:
+    """
+    This class for addition of Two Matrices
+    """
+    def __init__(self, first_matrix, second_matrix):
+        """
+        defind Constructor method to initialize instance variables
+        """
+        self.first_matrix = first_matrix
+        self.second_matrix = second_matrix
+
+    def adding_two_matrices(self):
+        result_matrix = []
+        for rows in range(len(self.first_matrix)):
+            row = []
+            for colm in range(len(self.first_matrix[0])):
+                row.append(self.first_matrix[rows][colm]
+                           + self.second_matrix[rows][colm])
+            result_matrix.append(row)
+        print("Addition of two Matrix is: ")
+        print(*result_matrix, sep='\n')
+
+
+try:
+    first_matrix_rows = int(
+        input("Enter the Number of rows you need in First Matrix : "))
+    first_matrix_colums = int(
+        input("Enter the Number of Columns you need in First Matrix : "))
+
+except ValueError:
+    print("Please enter correct rows and colums 'It Must Be Number' onlys")
+    sys.exit()
+
+else:
+    print("Enter the elements of First Matrix:")
+    # using list comprehension
+    first_matrix = [[int(
+        input()) for matrix_colum in range(
+        first_matrix_colums)] for matrix_row in range(first_matrix_rows)]
+    print("First Matrix is :")
+    for element in first_matrix:
+        print(element)
+try:
+    second_matrix_rows = int(
+        input("Enter the Number of rows you need in Second Matrix : "))
+    second_matrix_colums = int(
+        input("Enter the Number of Columns you need in Second Matrix : "))
+
+    if (first_matrix_rows != second_matrix_rows) and (
+            first_matrix_colums != second_matrix_colums):
+        print(
+            "Sorry! addition of two matrices is possible only if two matrices have same order! ")
+        sys.exit()
+
+except ValueError:
+    print("Please enter correct rows and colums 'It Must Be Number' onlys")
+    sys.exit()
+
+else:
+    print("Enter the elements of Second Matrix:")
+    second_matrix = [[int(input()) for matrix_colum in range(
+        second_matrix_colums)] for matrix_row in range(second_matrix_rows)]
+    print("Second Matrix is: ")
+    for element in second_matrix:
+        print(element)
+
+
+# first_matrix = [[12,7,3],
+#                 [4,5,6],
+#                 [7,8,9]]
+# second_matrix = [[5,8,1],
+#                  [6,7,3],
+#                  [4,5,9]]
+
+reference = AddingTwoMatrices(first_matrix, second_matrix)
+reference.adding_two_matrices()
+
+#
